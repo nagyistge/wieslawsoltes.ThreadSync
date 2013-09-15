22de2bb0a8249afb452659db394276b2ff090888
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-09-15 10:31:34 +0200

    Refactored Program

---
+++ DrawService.cs
@@ -0,0 +1,57 @@
+﻿#region References
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+#endregion
+
+namespace ThreadSync
+{
+    #region DrawService
+
+    public class DrawService<T>
+    {
+        #region Properties
+
+        public volatile object Sync = new object();
+        public bool IsRunning { get; private set; }
+        public T Data { get; set; }
+        public Action<T> Action { get; set; } 
+
+        #endregion
+
+        #region Running
+
+        public void SetRunning(bool run)
+        {
+            IsRunning = run;
+        }
+
+        #endregion
+
+        #region Draw Loop
+
+        public void Loop()
+        {
+            while (IsRunning)
+            {
+                lock (Sync)
+                {
+                    // execute drawing action
+                    if (Action != null)
+                        Action(Data);
+
+                    // wait for next frame notification
+                    Monitor.Wait(Sync);
+                }
+            }
+        } 
+
+        #endregion
+    }
+
+    #endregion
+}
